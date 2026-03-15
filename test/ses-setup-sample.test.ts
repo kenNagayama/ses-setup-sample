c4589398070ba9ef564c482c2e1fc35363303196
kenNagayama <k-nagayama@dicejreg.onmicrosoft.com>
2026-03-15 22:51:18 +0900

    Initial commit

---
+++ test/ses-setup-sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SesSetupSample from '../lib/ses-setup-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ses-setup-sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SesSetupSample.SesSetupSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
